3575c51e5ff3aa73e99b33f95d2a8ff521385de3
Benjamin Schulz <benjamin.schulz@du.edu>
2019-05-13 01:07:27 -0600

    Work on Content Component

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,39 +1,37 @@
-import React, { useEffect, useCallback } from "react"
+import React, { useEffect, useCallback, useState } from "react"
 // import ReactDOM from "react-dom"
 
 import styles from "./content.module.css"
 
 import AutoResponsive from "autoresponsive-react"
 
+const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const itemSizeDefault = 200;
+let itemStyles = {};
+let getItemStyle = function() {
+  return {
+    style:{
+      width: itemSizeDefault,
+      height: itemSizeDefault
+    }
+  };
+};
+
+items.forEach(i => {
+  itemStyles[i] = getItemStyle();
+});
+
 const Content = (props) => {
   let containerRef = React.createRef();
   let containerW = 0;
 
-  const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let itemStyles = {};
-  let getItemStyle = function() {
-    return {
-      style:{
-        width: 100,
-        height: 100,
-        cursor: 'pointer',
-        border: '1px solid #f2f2f2',
-        borderRadius: 5,
-        backgroundColor: '#424242',
-        lineHeight: '100px',
-        textAlign: 'center',
-        textShadow: '1px 1px 0px #816abe',
-        userSelect: 'none'
-      }
-    };
-  };
-
-  items.forEach(i => {
-    itemStyles[i] = getItemStyle();
-  });
+  const [itemStylesState, setItemStylesState] = useState(itemStyles);
 
   useEffect(() => {
+    console.log(containerRef);
     containerW = containerRef.current.offsetWidth;
+    console.log(containerW);
+
   }, []);
 
   const getAutoResponsiveProps = () => {
@@ -42,28 +40,32 @@ const Content = (props) => {
       containerWidth: containerW,
       itemClassName: 'item',
       gridWidth: 100,
-      transitionDuration: '.5'
+      transitionDuration: '.5',
+      transitionTimingFunction: 'easeIn'
     };
   };
 
-  const handleItemClick = (i) => {
+  const handleItemClick = useCallback((i) => {
     console.log('Item' + i + 'clicked');
-
-    if (itemStyles[i].width === '100px') {
-      itemStyles[i].width = '200px';
-      itemStyles[i].height = '200px';
+    console.log(itemStyles[i]);
+    console.log(itemStylesState[i]);
+    if (itemStyles[i].style.width === itemSizeDefault) {
+      itemStyles[i].style.width = itemSizeDefault * 2 + 10;
+      itemStyles[i].style.height = itemSizeDefault * 2 + 10;
     } else {
-      itemStyles[i].width = '100px';
-      itemStyles[i].height = '100px';
+      itemStyles[i].style.width = itemSizeDefault;
+      itemStyles[i].style.height = itemSizeDefault;
     }
-  };
+
+    setItemStylesState(itemStyles);
+  }, []);
 
   return (
     <div className={styles.container}>
       <AutoResponsive ref={containerRef} {...getAutoResponsiveProps()}>
 
         { items.map(i => {
-          return <div key={i} className={styles.item} {...itemStyles[i]} onClick={() => handleItemClick(i)}>Item {i}</div>;
+          return <div key={i} className={styles.item} {...itemStylesState[i]} onClick={() => handleItemClick(i)}>Item {i}</div>;
           })
         }
       </AutoResponsive>
