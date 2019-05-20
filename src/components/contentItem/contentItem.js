f8efefa6de9730e7a7c114d8827b791fc0f03ed3
Benjamin Schulz <benjamin.schulz@du.edu>
2019-05-20 17:48:47 -0600

    Work on Content Displaying

--- src/components/contentItem/contentItem.js
+++ src/components/contentItem/contentItem.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import styles from "./contentItem.module.css";
 
 const ContentItem = (props) => {
 
+  const [classState, setClassState] = useState(styles.item);
+
+  const handleClick = () => {
+    console.log('item clicked');
+  }
+
+  const handleBlur = () => {
+    console.log('item blurred');
+  }
+
   return (
-    <div className={styles.item} onClick={props.handleClick}>
+    <div className={classState} onClick={handleClick} onBlur={handleBlur}>
       <h3>{props.content}</h3>
       <a href='/' className={[styles.link, styles.left].join(' ')}>Link 1</a>
       <a href='/' className={[styles.link, styles.right].join(' ')}>Link 2</a>
