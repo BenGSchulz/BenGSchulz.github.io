bbb4841164efa405e8933bffb60d6a9d71eac1c8
BenGSchulz <benschulz.dev@gmail.com>
2024-12-19 16:47:44 -0700

    seo

---
+++ src/components/seo.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useSiteMetadata } from '../hooks/use-site-metadata';
+
+const SEO = ({
+  title,
+  description,
+  pathname,
+  children,
+}: {
+  title?: string;
+  description?: string;
+  pathname?: string;
+  children?: React.ReactNode;
+}) => {
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    siteUrl,
+  } = useSiteMetadata();
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname || ``}`,
+  };
+
+  return (
+    <>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      {children}
+    </>
+  );
+};
+
+export default SEO;
